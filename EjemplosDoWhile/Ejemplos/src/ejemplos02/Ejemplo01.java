11b0d31e6bc5adf9eb4c281fbe1317f69ed5bce0
valsam <mvsamaniego1@utpl.edu.ec>
2023-11-23 16:06:00 -0500

    Subir cambios

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo01.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo01.java
@@ -20,9 +20,16 @@ public class Ejemplo01 {
 
     public static void main(String[] args) {
         int contador = 1;
+        int suma = 0;
+        int numero = 1;
         do {
             
+           suma = suma + numero;
+           contador = contador + 1;
+            
         } while (contador <= 10);
+        
+        System.out.println(suma);
     }
 
 }
